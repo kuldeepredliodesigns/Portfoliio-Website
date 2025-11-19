742b5aea8a03b31c05bc9c7aacaec1ddbd5b0c75
Kuldeepsinh Jadeja <kuldeep.redliodesigns@gmail.com>
2025-11-19 19:17:55 +0530

    Refactor font usage and styles; remove unused assets and add new font files

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,10 @@
 import "../styles/globals.scss";
 import { ThemeProvider } from '../contexts/ThemeContext';
-import { Geist, Geist_Mono } from 'next/font/google';
-
-const geist = Geist({
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  subsets: ['latin'],
-})
-
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <div className={geist.className}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "../styles/Home.module.scss";
 import MainWrapper from "@/components/MainWrapper/MainWrapper";
 import Dock from "@/components/Dock/Dock";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
@@ -20,6 +9,7 @@ export default function Home() {
       <Dock />
       <div className={styles.mainWrapper}>
         <h1 className={styles.title}>Hi, I'm Kuldeepsinh Jadeja 👋</h1>
+        <p className={styles.aboutMe}>Passionate about Tech, Turned Software engineer❤️</p>
       </div>
     </>
   );
